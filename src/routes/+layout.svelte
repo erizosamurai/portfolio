6b1f8ab805886033cbd013b878202041b2b5dd51
Adhit <adhit.simhadri@gmail.com>
2025-07-06 02:44:41 +0530

    added about section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script>
 	import '../app.css';
 	import Sparkles from '$lib/components/ui/Sparkles/Sparkles.svelte';
+	import Sidebar from '$lib/components/Sidebar.svelte';
 	let { children } = $props();
 </script>
 
 {@render children()}
+
+
+
 
 <div class="relative min-h-screen overflow-hidden">
 	<!-- Sparkles in the background -->
